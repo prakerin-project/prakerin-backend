9475122d509cd4d6d5dc8a94b103fab2a3a4806e
arilramadani24 <arilramadani245@gmail.com>
2023-10-03 15:29:20 +0700

    feat: migration

---
+++ database/migrations/2023_10_03_073848_create_kelas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_jurusan')->nullable(false);
+            $table->char('kelompok', 1)->nullable();
+            $table->enum('tingkat', ['10', '11', '12'])->nullable(false);
+
+            $table->foreign('id_jurusan')->references('id')->on('jurusan')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
