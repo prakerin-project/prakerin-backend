ca542cde5d7522ef38bbfe544acb66d19f9583c4
Darwin Asus <d.f.winzi2006@gmail.com>
2023-10-03 16:12:21 +0700

    feat(migration): add more migration Walas, Siswa

---
+++ database/migrations/2023_10_03_084354_create_walas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('walas', function (Blueprint $table) {
+            /* ------------------------------- ATTRIBUTES ------------------------------- */
+            $table->string('nip', 20)->primary();
+            $table->unsignedBigInteger('id_kelas')->nullable(false);
+            $table->string('id_user', 36)->nullable(false);
+            $table->string('nama', 100)->nullable(false);
+            $table->string('no_telp', 22)->nullable(false); // (+62) 8XX-XXXX-XXXX
+            /* ----------------------------------- FK ----------------------------------- */
+            $table->foreign('id_kelas')->references('id')->on('kelas')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('id_user')->references('id')->on('user')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('walas');
+    }
+};
