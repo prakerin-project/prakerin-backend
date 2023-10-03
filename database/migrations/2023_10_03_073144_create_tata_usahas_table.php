1c5393e8f10c3744427ce289e6cea5eda201b355
Darwin Asus <d.f.winzi2006@gmail.com>
2023-10-03 14:51:16 +0700

    feat(migration): more migrations Pembimbing, TU, Hubin

---
+++ database/migrations/2023_10_03_073144_create_tata_usahas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tata_usaha', function (Blueprint $table) {
+            /* ------------------------------- ATTRIBUTES ------------------------------- */
+            $table->string('nip', 20)->primary();
+            $table->string('id_user', 36)->nullable(false);
+            $table->string('nama', 100)->nullable(false);
+            $table->string('no_telp', 22)->nullable(false); // (+62) 8XX-XXXX-XXXX
+            /* ----------------------------------- FK ----------------------------------- */
+            $table->foreign('id_user')->references('id')->on('user')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tata_usaha');
+    }
+};
