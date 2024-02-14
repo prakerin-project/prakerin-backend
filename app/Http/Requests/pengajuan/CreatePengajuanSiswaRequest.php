560c4c53161102016b303c82411596b9baf248c4
Darwin <139725726+MeDarwin@users.noreply.github.com>
2024-02-14 19:24:13 +0700

    refactor(validation): delete todo comment as it's alr done

--- app/Http/Requests/pengajuan/CreatePengajuanSiswaRequest.php
+++ app/Http/Requests/pengajuan/CreatePengajuanSiswaRequest.php
@@ -26,7 +26,6 @@ class CreatePengajuanSiswaRequest extends FormRequest
             /* -------------------------- INSTANCE OF PENGAJUAN ------------------------- */
             'nama_industri'     => ['required', 'string', 'max:100'],
             'alamat'            => ['required', 'string'],
-            //TODO: Add phone number regex
             'kontak_indsutri'   => ['required', 'string','regex:^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'],
             /* ---------------------------- INSTANCE OF SISWA --------------------------- */
             'nis_siswa'         => ['array', 'required']
